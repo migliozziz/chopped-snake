2cd20d827c8716f4ae25c7bc890304976023b122
migliozziz <migliozziz@gmail.com>
2013-09-16 12:13:32 -0400

    Create rm-files-with-timediff.py

---
+++ rm-files-with-timediff.py
@@ -0,0 +1,20 @@
+# Remove files in listed directories older than X days.
+
+import datetime
+import os
+from time import time
+
+today = datetime.date.today()
+now = time()
+days = 1
+dlist = [r"\\localhost\d$\folder1",
+         r"\\localhost\d$\folder2"]
+
+for d in dlist:
+    path = os.listdir(d)
+    for files in path:
+        fullpath = os.path.join(d, files)
+        if os.path.isfile(full):
+            timediff = (now - os.path.getmtime(full))/86400
+            if timediff > days:    # Older than x days.
+                os.remove(fullpath)
