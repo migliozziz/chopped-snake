47adb0aa960be1ddc7c29b97c564b355c16d9bdb
migliozziz <migliozziz@gmail.com>
2013-11-15 14:38:28 -0500

    Using commandline parameter, removed logging.
    Removed unused logging, simplified.

--- distribute-files-to-many-dirs.py
+++ distribute-files-to-many-dirs.py
@@ -1,76 +1,48 @@
-import logging
 import os
 import shutil
+import sys
 
 class Resources:
+    def __init__(self, resource, resDirsList):
+        self.resource 		= resource
+        self.resDirsList	= resDirsList
+        self.table 		= self._directory_table()
 
-	def __init__(self, watchDir, doneDir, resDirsList):
-		self.watchDir 		= watchDir # Watch Dir.
-		self.doneDir 		= doneDir # Post Processing Move to Dir.
-		self.resDirsList	= resDirsList # List of Root Resource Dirs.
-		self.table 		= self._directory_table() 
-		self._logging()
+    # Iterate through table and distribute matched resource.
+    def Main(self):
+        filename = os.path.basename(self.resource)
+        for key, relPath in self.table.iteritems():
+            if filename.startswith(key) or filename.endswith(key):
+                print os.path.basename(self.resource)
+                self._copy_resource(relPath)
+        return
 
-	def Main(self):
-		self.check_watch_directory()
+    # Returns a dictionary.
+    def _directory_table(self):
+        table = {# AFP/Mix mode
+            'X0': r"AFPDSIN\CODEDFNT",
+            'T1': r"AFPDSIN\CODEPAGE",
+            'C0': r"AFPDSIN\FNTCHAR",
+            'F1': r"AFPDSIN\FORMDEF",
+            'O1': r"AFPDSIN\OVERLAY",
+            'P1': r"AFPDSIN\PAGEDEF",
+            'S1': r"AFPDSIN\PAGESEG",
+            # LCDS
+            '.FNT' : r"Xchange\Xfont",
+            '.FRM' : r"Xchange\xform",
+            '.IMG' : r"Xchange\ximage",
+            '.LGO' : r"Xchange\xlogo" }
+        return table
 
-	def _logging(self):
-		logging.basicConfig(filename='Resources.log',
-                                        level=logging.INFO,
-                                        format='%(asctime)s \t%(message)s',
-                                        datefmt='%m/%d/%Y %H:%M:%S')
-
-	# Returns a dictionary.
-	def _directory_table(self):
-		# Key: Filename/File Extension
-		# Value: Relative path.
-		table = {# AFP/Mix mode
-                        'X0': "AFPDSIN/CODEDFNT",
-                        'T1': "AFPDSIN/CODEPAGE",
-                        'C0': "AFPDSIN/FNTCHAR",
-                        'F1': "AFPDSIN/FORMDEF",
-                        'O1': "AFPDSIN/OVERLAY",
-                        'P1': "AFPDSIN/PAGEDEF",
-                        'S1': "AFPDSIN/PAGESEG",
-                        # LCDS
-                        '.FNT' : "Xchange/Xfont",
-                        '.FRM' : "Xchange/xform",
-                        '.IMG' : "Xchange/ximage",
-                        '.LGO' : "Xchange/xlogo" }
-		return table
-
-	def check_watch_directory(self):
-		""" Iterate through each resource found in Watch Directory.
-		Iterate though table and if the resource is matched to key,
-		begin to copy resource.
-
-		"""
-		for resource in os.listdir(self.watchDir):
-			for key, relPath in self.table.iteritems():
-				if resource.startswith(key) or resource.endswith(key):
-					self._copy_resource(resource, relPath)
-
-	def _copy_resource(self, resource, relPath):
-		resPath = os.path.join(self.watchDir, resource)	
-		logging.info('Resource found: %s', resPath)
-		for targetResourcePath in self.resDirsList:
-			shutil.copy(resPath, os.path.join(targetResourcePath, relPath))
-			logging.info('Copied to: %s', os.path.join(targetResourcePath, relPath))
-		# Once resource has been copied to all locations, move the file from WatchDir.
-		self._move_resource(resPath, resource)
-
-	def _move_resource(self, resPath, resource):
-		try:
-                        shutil.move(resPath, self.doneDir)
-			logging.info('Distribution complete. Moving to: %s',
-			 		os.path.join(self.doneDir, resource))
-		except WindowsError:
-			pass	
+    def _copy_resource(self, relPath):
+        for targetResourcePath in self.resDirsList:
+            shutil.copy(self.resource, os.path.join(targetResourcePath,
+                                                    relPath))
+            print "Copied to: ", os.path.join(targetResourcePath, relPath)	
 
 if __name__ == '__main__':
 	# Create Production instance.
-	prod = Resources("//127.0.0.1/d$/Resources/Production/",
-	 		"//127.0.0.1/d$/Resources/Prod_done",
+	prod = Resources(sys.arg[1],
 			[	"//127.0.0.1/d$/Resources/Target204",
         			"//127.0.0.1/d$/Resources/Target514",
 				"//127.0.0.1/d$/Resources/Default"])
